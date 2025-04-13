6f1aa3b8b4ce13d019fa3442f42ad5ec4bd67e60
Hafeez <itshafiz.me@gmail.com>
2025-04-14 01:11:13 +0530

    mainfest .All issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,38 @@
 const CACHE_NAME = 'ffv-cache-v1';
 
+// self.addEventListener('install', event => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then(cache => {
+//                 // Cache essential files only
+//                 return cache.add('/')
+//                     .then(() => cache.add('/index.html'))
+//                     .then(() => cache.add('/manifest.json'))
+//                     .catch(error => {
+//                         console.error('Cache add error:', error);
+//                         // Continue installation even if caching fails
+//                         return Promise.resolve();
+//                     });
+//             })
+//     );
+// });
+
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                // Cache essential files only
-                return cache.add('/')
-                    .then(() => cache.add('/index.html'))
-                    .then(() => cache.add('/manifest.json'))
-                    .catch(error => {
-                        console.error('Cache add error:', error);
-                        // Continue installation even if caching fails
-                        return Promise.resolve();
-                    });
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/manifest.json'
+                ]).catch(error => {
+                    console.error('Cache addAll error:', error);
+                    return Promise.resolve(); // continue installation despite the error
+                });
             })
     );
 });
+
 
 self.addEventListener('fetch', event => {
     event.respondWith(
